3474ca80b31a0f52e21114e541cc2c36e1d2da81
113821_Pisoni_Catalina <113821@tecnicatura.frc.utn.edu.ar>
2024-11-17 12:42:44 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,7 +50,7 @@ export class AppComponent {
           routerLink: 'entries/auth-list',
         },
         {
-          label: 'Dashboards',
+          label: 'Reporte de Accesos',
           routerLink: 'entries/dashboard',
         },
       ],
