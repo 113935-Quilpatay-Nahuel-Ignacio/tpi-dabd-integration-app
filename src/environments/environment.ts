68b30a88eb997bedf9ed29d76574819bc537454e
Nahuel Ignacio Quilpatay - 113935 <113935@tecnicatura.frc.utn.edu.ar>
2024-11-18 20:46:31 -0300

    tunnel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,20 +3,20 @@ export const environment = {
   production: true,
   apis: {
     // URLs directas a los servicios de cada grupo con su docker-compose
-    accesses: 'http://localhost:8001/',
-    accounts: 'http://localhost:8002/',
-    addresses: 'http://localhost:8003/',
-    cadastre: 'http://localhost:8004/',
-    constructions: 'http://localhost:8005/',
-    contacts: 'http://localhost:8006/',
-    employees: 'http://localhost:8007/',
-    expenses: 'http://localhost:8008/',
-    inventory: 'http://localhost:8009/',
-    moderations: 'http://localhost:8010/',
-    notifications: 'http://localhost:8011/',
-    payments: 'http://localhost:8012/',
-    suppliers: 'http://localhost:8013/',
-    tickets: 'http://localhost:8014/',
-    users: 'http://localhost:8015/'
+    accesses: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    accounts: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    addresses: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    cadastre: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    constructions: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    contacts: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    employees: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    expenses: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    inventory: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    moderations: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    notifications: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    payments: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    suppliers: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    tickets: 'https://lbsm4xgt-8080.brs.devtunnels.ms/',
+    users: 'https://lbsm4xgt-8080.brs.devtunnels.ms/'
   }
 };
